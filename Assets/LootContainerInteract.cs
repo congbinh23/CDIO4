893908916b0d7fd594a7bdcc6cc57d30f0dd7767
phanduc111 <phanxuanduc@dtu.edu.vn>
2024-11-05 02:32:58 +0700

    Interactable objects

---
+++ Assets/LootContainerInteract.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class LootContainerInteract : Interactable
+{
+    [SerializeField] GameObject closedChest;
+    [SerializeField] GameObject openedChest;
+    [SerializeField] bool opened;
+    public override void Interact(Character character)
+    {
+        if (opened == false)
+        {
+            opened = true;
+            closedChest.SetActive(false);
+            openedChest.SetActive(true);
+        }
+    }
+}
